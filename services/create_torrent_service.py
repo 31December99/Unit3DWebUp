2bdb939eff60789dd616c86aff7cb70de0e22f0b
parzival <jw79985@gmail.com>
2026-02-08 01:18:45 +0100

    Add async torrent service in services

---
+++ services/create_torrent_service.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import os
+from multiprocessing import Pool, cpu_count, Manager
+from pathlib import Path
+
+from services.interfaces import TorrentServiceInterface
+from config.api_data import trackers_api_data
+from config.constants import MediaStatus
+from config.settings import Load
+from models.media import Media
+
+from fastapi import FastAPI
+import torf
+
+config_settings = Load().load_config()
+
+
+def worker(args) -> dict:
+    torrent_path, torrent_name, torrent_archive, trackers_list, progress_queue, job_id = args
+    archive = torrent_archive or '.'
+    os.makedirs(archive, exist_ok=True)
+    torrent_filepath: Path = (Path(archive) / trackers_list[0] / f"{torrent_name}.torrent")
+
+    announces = [
+        trackers_api_data[t.upper()]['announce']
+        for t in (trackers_list or []) if t.upper() in trackers_api_data
+    ]
+
+    mytorr = torf.Torrent(path=torrent_path, trackers=announces)
+    mytorr.comment = config_settings.user_preferences.TORRENT_COMMENT
+    mytorr.name = torrent_name
+    mytorr.created_by = ""
+    mytorr.private = True
+    mytorr.source = trackers_list[0] if trackers_list else None
+    mytorr.segments = 16 * 1024 * 1024
+
+    # The callback
+    def callback_progress(torrent, filepath, pieces_done, pieces_total):
+        progress = pieces_done / pieces_total * 100
+        progress_queue.put({'torrent': torrent_name, 'progress': progress, 'job_id': job_id})
+
+    mytorr.generate(callback=callback_progress, interval=1)
+    mytorr.write(torrent_filepath)
+    return {'status': '200', 'message': torrent_filepath, 'job_id': job_id}
+
+
+class MyTorrentService(TorrentServiceInterface):
+    def __init__(self, app: FastAPI):
+        self.app = app
+
+    def start(self, media_list: list[Media], batch_size=16, workers: int = 4, progress_queue=None):
+        """
+        :param media_list: list of Media objects to process
+        :param batch_size: numer of item per batch
+        :param workers: number of workers
+        :param progress_queue: a safe queue where put the progress
+        :return:
+        """
+        results = []
+        # Sort by Size ( start with the smallest)
+        media_list.sort(key=lambda m: m.size)
+
+        # Create one or more batch list and call the function
+        for i in range(0, len(media_list), batch_size):
+            batch = media_list[i:i + batch_size]
+            batch_results = self._create_batch(batch, trackers=['ITT'], workers=workers, progress_queue=progress_queue)
+            results.extend(batch_results)
+        return results
+
+    def _create_batch(self, media_list: list[Media], trackers: list[str] = None, workers: int = None,
+                      progress_queue=None):
+        workers = workers or cpu_count()
+        archive_path = config_settings.user_preferences.TORRENT_ARCHIVE_PATH or '.'
+
+        with Manager() as manager:
+            if progress_queue is None:
+                progress_queue = manager.Queue()
+
+            # Create a list of task only for Media with description and video status positive
+            jobs = [
+                (m.torrent_path, m.torrent_name, archive_path, trackers, progress_queue, m.job_id)
+                for m in media_list if m.status not in (MediaStatus.DESCRIPTION_ERROR, MediaStatus.VIDEO_ERROR)
+            ]
+
+            # Go
+            with Pool(workers) as pool:
+                results = pool.map(worker, jobs)
+
+            # Wait for the end
+            while not progress_queue.empty():
+                progress_update = progress_queue.get()
+                print(f"Torrent {progress_update['torrent']}: {progress_update['progress']:.2f}%")
+
+        return results
---
+++ services/torrent_service.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import asyncio
+
+from services.create_torrent_service import MyTorrentService
+from models.media import Media
+from threading import Thread
+from fastapi import FastAPI
+from multiprocessing import Manager
+
+
+class TorrentService:
+    """
+        Accept a list of Media objects and create torrents in batch mode
+        TODO For the moment n° workers and batch_size are hardcoded
+    """
+
+    def __init__(self, media_list: list[Media], app: FastAPI):
+        """
+        :param media_list: a list of Media objects
+        :param app: FastApi
+        """
+
+        # Process media
+        self.media_list = media_list
+
+        # FastApi instance app
+        self.app = app
+
+        # Torrent creation
+        self.torr_service = MyTorrentService(app=app)
+
+        # Use the queue to get the progress % during the torrent creation process
+        self.manager = Manager()
+        self.progress_queue = self.manager.Queue()
+        self.result_queue = self.manager.Queue()
+
+    def run_batch(self):
+        """
+             Try to process group of files to avoid ssd saturation
+        :return:
+        """
+
+        #TODO: ha senso spingere con i workers se il mio ssd lavoro già al 100%?
+        try:
+            results = self.torr_service.start(self.media_list, batch_size=16, workers=4,
+                                                    progress_queue=self.progress_queue)
+            self.result_queue.put(results)
+        except Exception as e:
+            print(e)
+
+    async def start(self):
+        """
+            Start the torrent service
+        :return:
+        """
+
+        # Run run_batch() in background
+        thread = Thread(target=self.run_batch)
+        thread.start()
+
+        # Keep looping while the thread is running or the queue is not empty
+        all_progress = {}
+        while thread.is_alive() or not self.progress_queue.empty():
+            while not self.progress_queue.empty():
+                # The worker fill the queue, here we consume it and forward the progress to the client
+                update = self.progress_queue.get()
+
+                # For each torrent send the current progress...
+                all_progress[update['torrent']] = update['progress']
+                await self.app.state.ws_manager.broadcast({
+                    "type": "progress",
+                    "level": "progress",
+                    "job_id": update['job_id'],
+                    "process": "Torrent",
+                    "progress": update['progress'],
+                    "message": f"[New torrent] {update['torrent']} - {round(update['progress'], 2)}",
+                })
+
+                print("log", "progress", update['job_id'], update['progress'],
+                      f"[New torrent] {update['torrent']} - {round(update['progress'], 2)}")
+
+            await asyncio.sleep(0.1)
+
+        thread.join()
+        results = self.result_queue.get()
+
+        # Invio log finali
+        for torrent in results:
+            if torrent.get('status') == '200':
+                await self.app.state.ws_manager.broadcast({
+                    "type": "progress",
+                    "level": "progress",
+                    "job_id": torrent['job_id'],
+                    "process": "Completed",
+                    "progress": 100.0,
+                    "message": f"[New torrent] {torrent['message'].name} from {torrent['message'].parent}"
+                })
+
+            if torrent.get('status') == '409':
+                await self.app.state.ws_manager.broadcast({
+                    "type": "progress",
+                    "level": "progress",
+                    "job_id": torrent['job_id'],
+                    "process": "Error",
+                    "progress": 100.0,
+                    "message": f"[Reloaded] {torrent['message'].name} from {torrent['message'].parent}"
+                })
