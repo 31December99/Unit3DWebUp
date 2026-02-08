0d54da16e4227d37dac7282b759f27896353e923
parzival <jw79985@gmail.com>
2026-02-08 22:27:28 +0100

    Fix constructor in seed_usecase.py to allow dependency inj

--- use_case/seed_usecase.py
+++ use_case/seed_usecase.py
@@ -9,24 +9,27 @@ from fastapi import FastAPI
 
 
 class SeedUseCase:
-    def __init__(self, app: FastAPI, client: str, media_list: list[Media] | None = None, job_id: str | None = None):
+    def __init__(self, app: FastAPI, client: str, job_id: str | None = None):
         """
         :param app: the fastapi app
         :param job_id: the job id
         :param client: torrent client name
         """
-        self.media_list = media_list
+        self.media_list = None
         self.app = app
         self.job_id = job_id
         self.client = client
 
-    async def execute(self) -> bool:
+    async def execute(self, media_list: list[Media] | None = None) -> bool:
         """
         Execute the seed use case : load one or more jobs, login to client, verify torrent files,
                                     add torrents file to the client
+
+        :param media_list: list of media to upload used when shared with other services
         :return:
         """
 
+        self.media_list = media_list
         # Create the media list for single torrent
         if self.job_id and not self.media_list:
             results = await self.app.state.job.get_job(job_id=self.job_id)
