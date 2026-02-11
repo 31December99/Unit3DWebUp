13c4a0fbe94adc30c20eda6b8546370a31d6f971
parzival <jw79985@gmail.com>
2026-02-11 10:34:14 +0100

    Add a custom widget to create a lateral menu to navigate to the selected page in navigation_rail_widget.dart

---
+++ UI/lib/widgets/navigation_rail_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:UI/providers/providers.dart';
+
+/// Separate widget uses NavigationRail to build a lateral menù
+/// The user can click on a specific icon to navigate to the selected page
+class NavigationSwitchText extends StatelessWidget {
+  const NavigationSwitchText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final posterProvider = context.watch<PosterProvider>();
+    return Text(posterProvider.selectedSource);
+  }
+}
+
+class NavigationRailMenu extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onSelect;
+
+  const NavigationRailMenu({
+    super.key,
+    required this.selectedIndex,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: onSelect,
+      labelType: NavigationRailLabelType.selected,
+      destinations: [
+
+        NavigationRailDestination(
+          icon: SvgPicture.asset('lib/assets/home-svgrepo-com.svg', width: 20),
+          selectedIcon: SvgPicture.asset(
+            'lib/assets/home-svgrepo-com.svg',
+            width: 20,
+          ),
+          label: const Text('HOME'),
+        ),
+
+        NavigationRailDestination(
+          icon: SvgPicture.asset(
+            'lib/assets/adjust-svgrepo-com.svg',
+            width: 35,
+          ),
+          selectedIcon: SvgPicture.asset(
+            'lib/assets/adjust-svgrepo-com.svg',
+            width: 35,
+          ),
+          label: const Text('Settings'),
+        ),
+
+        NavigationRailDestination(
+          icon: SvgPicture.asset(
+            'lib/assets/ladybug-bug-svgrepo-com.svg',
+            width: 25,
+          ),
+          selectedIcon: SvgPicture.asset(
+            'lib/assets/ladybug-bug-svgrepo-com.svg',
+            width: 25,
+          ),
+          label: const Text('Jobs'),
+        ),
+      ],
+    );
+  }
+}
