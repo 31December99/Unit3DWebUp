578429f4c1121fbfcb299a3c65448b895fb044ab
parzival <jw79985@gmail.com>
2026-02-11 14:08:05 +0100

    Add SettingPage in setting.dart

---
+++ UI/lib/pages/setting.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:UI/widgets/settings/setting_container.dart';
+
+/// Classe per leggere la configurazione dal backend (SettingPage)
+///
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(padding: EdgeInsets.all(16.0)),
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [const SizedBox(width: 8)],
+                ),
+                const SizedBox(height: 8),
+                const Expanded(child: Setting()),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
