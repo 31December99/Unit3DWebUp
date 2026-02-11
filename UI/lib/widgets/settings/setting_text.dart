dca675f4a133b9c7c919fd4549bca346d7caa2b0
parzival <jw79985@gmail.com>
2026-02-11 14:17:04 +0100

    Add a custom TexField widget for SettingPage setting_text.dart

---
+++ UI/lib/widgets/settings/setting_text.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+/// Custom widget for a TextField in the SettingsPage
+class SettingText extends StatefulWidget {
+  final String label;
+  final String value;
+  final ValueChanged<String> onChanged;
+  final String? hint;
+
+  const SettingText({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.onChanged,
+    this.hint,
+  });
+
+  @override
+  State<SettingText> createState() => _SettingTextState();
+}
+
+class _SettingTextState extends State<SettingText> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.value);
+  }
+
+  @override
+  void didUpdateWidget(covariant SettingText oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Check the previous state !
+    if (widget.value != oldWidget.value) {
+      _controller.text = widget.value;
+      _controller.selection = TextSelection.fromPosition(
+        TextPosition(offset: _controller.text.length),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: Row(
+        children: [
+          Expanded(flex: 2, child: Text(widget.label)),
+          const SizedBox(width: 12),
+          Expanded(
+            flex: 3,
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: widget.hint,
+                border: OutlineInputBorder(),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 4,
+                ),
+              ),
+              onChanged: widget.onChanged,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
