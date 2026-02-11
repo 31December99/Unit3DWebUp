c26ab01f509973cd5ca10e0911b0dbe29d748d8e
parzival <jw79985@gmail.com>
2026-02-11 14:16:33 +0100

    Add a custom switchButton for SettingPage in setting_option.dart

---
+++ UI/lib/widgets/settings/setting_switch.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+/// Custom widget to represent a switch in the SettingPage
+class SettingSwitch extends StatelessWidget {
+  final String label;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  const SettingSwitch({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label),
+        Switch(value: value, onChanged: onChanged),
+      ],
+    );
+  }
+}
