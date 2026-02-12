0e0a1a4ffb255a3d73664ef1f0b763cfce629893
parzival <jw79985@gmail.com>
2026-02-12 17:08:18 +0100

    Add colors to logger.py

--- config/logger.py
+++ config/logger.py
@@ -3,13 +3,35 @@
 import logging
 import sys
 
+COLORS = {
+    logging.DEBUG: "\033[36m",     # ->  cyano
+    logging.INFO: "\033[32m",      # -> green
+    logging.WARNING: "\033[33m",   # -> yellow
+    logging.ERROR: "\033[31m",     # -> red
+    logging.CRITICAL: "\033[41m",  # -> red background
+}
+
+RESET = "\033[0m"
+
+class ColorFormatter(logging.Formatter):
+
+    # Called by the handler
+    def format(self, record):
+        color = COLORS.get(record.levelno, "")
+        message = super().format(record)
+        return f"{color}{message}{RESET}"
+
+
 # /// Attempt to create a dedicated logger for Docker....
 def get_logger(name: str = "app_logger") -> logging.Logger:
     logger = logging.getLogger(name)
+
     if not logger.hasHandlers():
         logger.setLevel(logging.INFO)
         handler = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
-        handler.setFormatter(formatter)
+        formatter = ColorFormatter("[%(levelname)s] %(name)s: %(message)s")
+        handler.setFormatter(formatter) # Custom formatter
         logger.addHandler(handler)
     return logger
+
+
