f04829799a2db56e70f0692779640659cdc96349
parzival <jw79985@gmail.com>
2026-04-21 20:33:01 +0200

    revert server port

--- start.py
+++ start.py
@@ -737,7 +737,7 @@ async def filter_search(payload: FilterRequest):
 
 
 def main():
-    uvicorn.run("start:app", host="127.0.0.1", port=10000, reload=False)
+    uvicorn.run("start:app", host="127.0.0.1", port=8000, reload=False)
 
 
 if __name__ == "__main__":
